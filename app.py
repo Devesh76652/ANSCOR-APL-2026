bea567f5ba25072c51690119a752d6150d7092e5
Devesh76652 <deveshtatale@gmail.com>
2026-06-01 18:21:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -23,11 +23,11 @@ GITHUB_RAW_BASE = "https://raw.githubusercontent.com/Anscortournament/APL/main/"
 # Team Database
 TEAM_DB = {
     "Capital Chellengers": {
-        "remote": GITHUB_RAW_BASE + "CapitalChellengers.jpeg",
+        "remote": GITHUB_RAW_BASE + "Capital Chellengers.jpeg",
         "squad": ["Umesh sutar", "Kisan Pawar", "Imran Khan", "Pooja Gaikwad", "Rohan Mhatre", "Saurabh Padad", "Vijayaraj Yadav", "Vaibhav Sonawane", "Azad kanojiya", "Shrushti Thali", "Gaurav Singh", "Siddhesh A"],
     },
     "Black panther": {
-        "remote": GITHUB_RAW_BASE + "Blackpanther.jpeg",
+        "remote": GITHUB_RAW_BASE + "Black Panther.jpeg",
         "squad": ["Vishal Rajput", "Hitesh Purohit", "Omprakash Ashok Kamble", "Daraksha Khan", "Rohan vaity", "Devesh Tatale", "Suvarna Gupta", "Sanjay Sakpal", "SUMIIT M MORASKAR", "PRADEEP SHRIVASTAV", "Ishwar", "Rakesh Mishra", "Akash nagade"],
     },
     "Super Kings": {
