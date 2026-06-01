2dcf8f0259620244b4ad3f8bdd9be255294222e6
Devesh76652 <deveshtatale@gmail.com>
2026-06-01 17:01:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ TOURNAMENT_LOGO_FILE = "image_4d6904.png"
 
 # Team Database
 TEAM_DB = {
-    "Capital Challengers": {
+    "CapitalChallengers": {
         "local": "CapitalChallengers.jpeg",
         "remote": GITHUB_RAW_BASE + "CapitalChallengers.jpeg",
         "squad": ["Umesh sutar", "Kisan Pawar", "Imran Khan", "Pooja Gaikwad", "Rohan Mhatre", "Saurabh Padad", "Vijayaraj Yadav", "Vaibhav Sonawane", "Azad kanojiya", "Shrushti Thali", "Gaurav Singh", "Siddhesh A"],
